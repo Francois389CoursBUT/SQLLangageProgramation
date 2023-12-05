abe829c75420e1647eee6db50e971173d700bc8d
François SP <francois-sp@gmx.fr>
2023-12-05 18:26:59 +0100

    TP3

--- TP3/TP3_Generation_Facture.sql
+++ TP3/TP3_Generation_Facture.sql
@@ -31,3 +31,54 @@ END; //
 
 DELIMITER ;
 SELECT genereFacture(1) ; -- Test de la génération d'une facture
+SELECT genereFacture(1) ; -- Test de la génération d'une facture
+
+-- Etape 3 fonction genererCodeBarre
+DELIMITER //
+DROP PROCEDURE IF EXISTS getLastegencodeAndUdate //
+CREATE PROCEDURE getLastegencodeAndUdate (out code_produit VARCHAR(5))
+BEGIN
+DECLARE code_produit VARCHAR(5); -- Numéro du produit du fabricant
+    START TRANSACTION ;
+    SELECT CONTENU_N INTO code_produit FROM parametres WHERE ID = 'LAST_GENCODE';
+    -- Mise à jour du paramètre LAST_GENCODE
+    UPDATE parametres SET CONTENU_N = CONTENU_N + 1 WHERE ID = 'LAST_GENCODE';
+    COMMIT ;
+END //
+
+DROP function IF EXISTS genererCodeBarre //
+CREATE FUNCTION genererCodeBarre ()
+RETURNS VARCHAR(13)
+BEGIN
+    DECLARE resultat VARCHAR(13) ;
+    DECLARE deb_gencode VARCHAR(7); -- Début du code barre
+    DECLARE code_produit VARCHAR(5); -- Numéro du produit du fabricant
+    DECLARE cle_control int; -- Clé de contrôle
+    DECLARE somme_rang_pair int; -- Clé de contrôle
+    DECLARE somme_rang_impair int; -- Clé de contrôle
+
+    SELECT CONTENU_A  INTO deb_gencode FROM parametres WHERE ID = 'DEB_GENCODE';
+    CALL getLastegencodeAndUdate(@code_produit);
+    SET code_produit = @code_produit;
+
+    SET code_produit = LPAD(code_produit, 5, '0');
+
+    -- Calcul de la clé de contrôle
+    -- 1. Somme des chiffres de rang pair du code barre
+    SET somme_rang_pair = SUBSTRING(deb_gencode, 2,1) + SUBSTRING(deb_gencode, 4,1) + SUBSTRING(deb_gencode, 6,1) + SUBSTRING(code_produit, 1,1) + SUBSTRING(code_produit, 3,1) + SUBSTRING(code_produit, 5,1);
+    -- 2. Multiplier par 3
+    SET somme_rang_pair = somme_rang_pair * 3;
+    -- 3. Somme des chiffres de rang impair du code barre
+    SET somme_rang_impair = SUBSTRING(deb_gencode, 1,1) + SUBSTRING(deb_gencode, 3,1) + SUBSTRING(deb_gencode, 5,1) + SUBSTRING(deb_gencode, 7,1) + SUBSTRING(code_produit, 2,1) + SUBSTRING(code_produit, 4,1);
+    -- 4. Somme des résultats des étapes 2 et 3 et le complément à 10 de cette somme modulo 10
+    SET cle_control = RIGHT(10 - ((somme_rang_pair + somme_rang_impair)  % 10), 1);
+
+    -- Concaténation du code barre
+    SET resultat = CONCAT(deb_gencode, code_produit, cle_control);
+    RETURN resultat;
+END //
+SELECT genererCodeBarre() ; -- Test de la génération d'un code barre
+-- TODO fix bug : Data truncation: Data too long for column 'resultat' at row 1
+-- Etape 5 - Générer les coes barres pour toutes les lignes de stockprix
+
+UPDATE stockprix SET CODE_BARRE = genererCodeBarre()
